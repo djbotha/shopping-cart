8fc5a82959a2d4eb3179c8c7812124c2827303fb
Daniel Jacobus Botha <20078196@sun.ac.za>
2017-06-03 21:50:35 +0200

    Added container for body

--- app.js
+++ app.js
@@ -4,15 +4,19 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var expressHbs = require('express-handlebars');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
 // view engine setup
+// app.engine('.hbs', expressHbs({defaultLayout: 'layout', extname: '.hbs'}));
+// app.set('view engine', '.hbs');
+
+app.engine('.hbs', expressHbs({partialsDir: __dirname + '/views/partials/', extname: '.hbs', defaultLayout: 'layout', layoutsDir: __dirname + '/views/layouts/'}));
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set('view engine', '.hbs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -23,7 +27,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
