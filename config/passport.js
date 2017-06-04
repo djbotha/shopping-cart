04e96030c7a164b6feb1d74ebdc80383a4c0f954
Daniel Jacobus Botha <20078196@sun.ac.za>
2017-06-04 13:59:50 +0200

    signin

--- config/passport.js
+++ config/passport.js
@@ -21,9 +21,9 @@ passport.use('local.signup', new LocalStrategy({
   req.checkBody('password', 'Invalid password').isLength({min: 4});
 
   var errors = req.validationErrors();
-  if( errors) {
+  if (errors) {
     var messages = [];
-    errors.forEach(function(error) {
+    errors.forEach(function (error) {
       messages.push(error.msg);
     });
     return done(null, false, req.flash('error', messages));
@@ -44,6 +44,36 @@ passport.use('local.signup', new LocalStrategy({
         return done(err);
       }
       return done(null, newUser);
-    })
+    });
+  });
+}));
+
+passport.use('local.signin', new LocalStrategy({
+  usernameField: 'email',
+  passwordField: 'password',
+  passReqToCallback: true
+}, function (req, email, password, done) {
+  req.checkBody('email', 'Invalid email').notEmpty().isEmail();
+  req.checkBody('password', 'Invalid password').notEmpty();
+
+  var errors = req.validationErrors();
+  if (errors) {
+    var messages = [];
+    errors.forEach(function (error) {
+      messages.push(error.msg);
+    });
+    return done(null, false, req.flash('error', messages));
+  }
+  User.findOne({'email': email}, function (err, user) {
+    if (err) {
+      return done(err);
+    }
+    if (!user) {
+      return done(null, false, {message: 'No user found.'});
+    }
+    if (!user.validPassword(password)) {
+      return done(null, false, {message: 'Invalid password.'});
+    }
+    return done(null, user);
   });
 }));
